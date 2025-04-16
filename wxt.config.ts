8e419407627eb624e3ddd28a435aab81e6401c3f
shenxin <wb_shenxin@meituan.com>
2025-04-16 17:19:03 +0800

    feat: 添加了新的依赖和配置 - 在 package.json 中添加了 @tanstack/vue-query、@webext-core/messaging、@webext-core/proxy-service 等依赖 - 在 pnpm-lock.yaml 中更新了依赖版本 - 在 wxt.config.ts 中配置了 Tailwindcss 和 unplugin-icons 插件 - 修改了 App.vue 以使用 Tailwindcss 样式 - 删除了不再使用的 style.css 文件

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,43 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
+import Icons from "unplugin-icons/vite";
+import Tailwindcss from "@tailwindcss/vite";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-vue'],
+  experimental: {
+    entrypointImporter: "vite-node",
+  },
+  modules: [
+    "@wxt-dev/module-vue",
+    "@wxt-dev/auto-icons",
+  ],
+  imports: {
+    presets: ["vue-router"],
+    imports: [
+      { from: "@tanstack/vue-query", name: "useQuery" },
+      { from: "@tanstack/vue-query", name: "useMutation" },
+    ],
+  },
+  runner: {
+    // disabled: true,
+  },
+  vite: () => ({
+    plugins: [Icons({ compiler: "vue3" }), Tailwindcss() as any],
+    ssr: {
+      // List any dependencies that depend on webextension-polyfill here for vite-node importer to work
+      noExternal: ["@webext-core/proxy-service", "@webext-core/messaging"],
+    },
+  }),
+  manifest: ({ browser }) => {
+    const permissions = ["storage", "tabs"];
+
+    return {
+      name: "weather-extension",
+      description: "weather-extension",
+      permissions,
+      host_permissions: ["<all_urls>"],
+    };
+  },
+  autoIcons: {
+    grayscaleOnDevelopment: false,
+  },
 });
